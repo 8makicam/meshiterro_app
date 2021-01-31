57e2581f82021b710afc07041c360c29e7179c14
Yuta Yamaki <8makicam@gmail.com>
2021-01-31 06:06:40 +0000

    3章終了

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '632026c364e9f205b35166f904dd83eb70b92a187df56bc02d78e97514d8192c2ae544b73a841a626285fed6bb9217ee6a548e5414552ffc796aa85a3dc70d95'
